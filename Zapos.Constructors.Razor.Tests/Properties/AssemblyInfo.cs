92789f06caecc2684dfea0eac2b5b047a5550444
Pavel Zaitsau <pavel.zaitsau@hotmail.com>
2013-06-06 20:43:49 +0300

    Remove version from Zapos.Constructors.Razor.Tests assembly.

--- Zapos.Constructors.Razor.Tests/Properties/AssemblyInfo.cs
+++ Zapos.Constructors.Razor.Tests/Properties/AssemblyInfo.cs
@@ -4,5 +4,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Zapos.Constructors.Razor.Tests")]
 [assembly: AssemblyProduct("Zapos.Constructors.Razor.Tests")]
 [assembly: Guid("17861ec5-a88e-45d0-8672-0096ecf0db9a")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
